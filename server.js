f520f548dcec6e77b73f9692f9617c27fbd05e81
ethandb23 <119861532+ethandb23@users.noreply.github.com>
2023-02-08 06:08:20 -1000

    Delete addons

--- server.js
+++ server.js
@@ -16,32 +16,3 @@ db.once('open', function() {
   console.log('Connected to MongoDB');
 });
 
-
-
-/** 
- * Use the app.use() method to associate the module with the '/steps' route.
- 
-app.use('/steps', stepsRoute);
- 
-* Create an express application for Steps
- * 
- * */ 
-
-
-
-/**
- * 
- * The app listens on port 3000 and logs a message to the console to indicate that the server is running.
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-
- *  */ 
-
-
-// ... the rest of our Express application code
-
-app.listen(3000, () => {
-  console.log('Express server started on port 3000');
-});
