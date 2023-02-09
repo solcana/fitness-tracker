f8d7ee147f3cdb27069c06c70b47d024df27564b
Hal Evans <hal.evans@newtoneurope.com>
2023-02-09 12:41:12 +0000

    cleaned up server.js, added comments, reordered code to match Usmans demo, added db to connection console log

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 // Require necessary NPM Packages
 const express = require("express");
 const mongoose = require("mongoose");
-const { Workout, Exercise } = require("./models/workout");
-const User = require("./models/user");
-const workoutSeedData = require('./seedData/workoutSeed');
-const userSeedData = require("./seedData/userSeed");
-
-const app = express();
 
 // Require DB Configuration File
 const db = require("./config/db");
@@ -16,16 +10,24 @@ mongoose.connect(db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-mongoose.connection.once("open", () => console.log("Connection to MongoDB"));
+mongoose.connection.once("open", () => console.log(`Connection to MongoDB on ${db}`));
 
 // Require Route Files
 const indexRouter = require("./routes/index");
 
-// Routes
+// Instantiate Express Application Object
+const app = express();
+
+// Define PORT for the API to run on
+const port = process.env.PORT || 5001;
+
+/** Middleware */
+// ADD HERE
+
+/** Routes */
 // Mount imported Routers
 app.use(indexRouter);
 
-const port = process.env.PORT || 5001;
 
 // Returns a statement saying the App is listening on our specified port
 app.listen(port, () => console.log(`App is listening on port ${port}`));
