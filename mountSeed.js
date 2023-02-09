2dce6520ae5633e55b7737dbc0d875b84080eccd
Hal Evans <hal.evans@newtoneurope.com>
2023-02-09 12:31:24 +0000

    created mountSeed.js to add the seed data to the database on running

---
+++ mountSeed.js
@@ -0,0 +1,132 @@
+// Require necessary NPM Packages
+const mongoose = require("mongoose");
+
+// Require seed data files
+const workoutSeedData = require('./seedData/workoutSeed');
+const userSeedData = require("./seedData/userSeed");
+
+// Require models
+const { Workout, Exercise } = require("./models/workout");
+const User = require("./models/user");
+
+// Require DB Configuration File
+const db = require("./config/db");
+
+// Connect to MongoDB database
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+    console.log("Connection to MongoDB");
+  
+    /**
+     * Seed Data inserted below
+     */
+  
+    // Adding Workout seed data to database
+    Workout.insertMany(workoutSeedData, (err, workouts) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('added workout seed data', workouts);
+      }
+  
+      // Adding User seed data to database
+      User.insertMany(userSeedData, (err, users) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('added user seed data', users);
+        }
+  
+        mongoose.connection.close();
+      });
+    });
+  });
+
+
+// mongoose.connection.once("open", () => console.log("Connection to MongoDB"));
+
+
+// /**
+//  * Seed Data inserted below
+//  */
+
+// // Adding Workout seed data to database
+// Workout.insertMany(workoutSeedData, (err, workouts) => {
+// if (err) {
+//     console.log(err);
+// } else {
+//     console.log('added workout seed data', workouts);
+// }
+// // mongoose.connection.close();
+// });
+
+// // Adding User seed data to database
+// User.insertMany(userSeedData, (err, users) => {
+// if (err) {
+//     console.log(err);
+// } else {
+//     console.log('added user seed data', users);
+// }
+// // mongoose.connection.close();
+// });
+
+// mongoose.connection.on('close', () => {
+//     console.log('Connection to MongoDB closed');
+// });
+
+/**
+ * Testing individual entries into database
+ */
+
+// const squat = {
+//     name: "Squat",
+//     weight: 10,
+//     reps: 10,
+//   };
+  
+// Exercise.create(squat, (err, exercise) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("added provided exercise data", exercise);
+//   }
+//   mongoose.connection.close();
+// });
+  
+//   const mondayWorkout = {
+//     startDate: new Date(2000, 01, 12, 14, 33, 12),
+//     completed: true,
+//     exercises: [],
+//   };
+  
+//   mondayWorkout.exercises.push(squat);
+  
+//   const user1 = {
+//     firstName: "usman",
+//     lastName: "bashir",
+//     username: "usbashir",
+//     password: "hfghghg",
+//   };
+  
+// User.create(user1, (err, user) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("added provided exercise data", user);
+//   }
+//   mongoose.connection.close();
+// });
+
+// Workout.create(mondayWorkout, (err, workout) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("added provided exercise data", workout);
+//   }
+//   mongoose.connection.close();
+// });
+  
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { Workout, Exercise } = require("./models/workout");
+const User = require("./models/user");
 const workoutSeedData = require('./seedData/workoutSeed');
 const userSeedData = require("./seedData/userSeed");
 
@@ -19,77 +20,12 @@ mongoose.connection.once("open", () => console.log("Connection to MongoDB"));
 
 // Require Route Files
 const indexRouter = require("./routes/index");
-const User = require("./models/user");
 
 // Routes
 // Mount imported Routers
 app.use(indexRouter);
 
 const port = process.env.PORT || 5001;
-
-const squat = {
-  name: "Squat",
-  weight: 10,
-  reps: 10,
-};
-
-// Exercise.create(squat, (err, exercise) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("added provided exercise data", exercise);
-//   }
-//   mongoose.connection.close();
-// });
-
-const mondayWorkout = {
-  startDate: new Date(2000, 01, 12, 14, 33, 12),
-  completed: true,
-  exercises: [],
-};
-
-mondayWorkout.exercises.push(squat);
-
-const user1 = {
-  firstName: "usman",
-  lastName: "bashir",
-  username: "usbashir",
-  password: "hfghghg",
-};
-
-// User.create(user1, (err, user) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("added provided exercise data", user);
-//   }
-//   mongoose.connection.close();
-// });
-
-// Workout.create(mondayWorkout, (err, workout) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("added provided exercise data", workout);
-//   }
-//   mongoose.connection.close();
-// });
-
-// Workout.insertMany(workoutSeedData, (err, workouts) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log('added workout seed data', workouts);
-//   mongoose.connection.close();
-// });
-
-// User.insertMany(userSeedData, (err, users) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log('added user seed data', users);
-//   mongoose.connection.close();
-// });
 
 // Returns a statement saying the App is listening on our specified port
 app.listen(port, () => console.log(`App is listening on port ${port}`));
