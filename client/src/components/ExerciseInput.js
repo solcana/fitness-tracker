fd5bef59ff2369fa7effb4a64ff0a5a63dd0de3e
Ana Solcan <solcan.anna@gmail.com>
2023-02-10 18:08:20 +0100

    corrected conflicts

--- client/src/components/ExerciseInput.js
+++ client/src/components/ExerciseInput.js
@@ -1,19 +1,14 @@
-// Hal
-
 import React, { Component } from "react";
 import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
 
-<<<<<<< HEAD:client/src/components/Exercise.js
-class Exercise extends Component {
-  state = {};
+class ExerciseInput extends Component {
   render() {
     return (
       <>
-        <h4>Exercise</h4>
         <Container>
           <Form>
             <Row className="align-items-center">
-              <Col sm={3} className="my-1">
+              <Col sm={4} className="my-1">
                 <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
                   Exercise Name
                 </Form.Label>
@@ -22,7 +17,7 @@ class Exercise extends Component {
                   placeholder="Exercise Name"
                 />
               </Col>
-              <Col sm={3} className="my-1">
+              <Col sm={2} className="my-1">
                 <Form.Label htmlFor="inlineFormInputGroupWeight" visuallyHidden>
                   Weight
                 </Form.Label>
@@ -30,10 +25,13 @@ class Exercise extends Component {
                   <InputGroup.Text>
                     <i className="fa-solid fa-weight-hanging"></i>
                   </InputGroup.Text>
-                  <CounterInput />
+                  <Form.Control
+                    id="inlineFormInputGroupWeight"
+                    placeholder="Weight"
+                  />
                 </InputGroup>
               </Col>
-              <Col sm={3} className="my-1">
+              <Col sm={2} className="my-1">
                 <Form.Label htmlFor="inlineFormInputGroupReps" visuallyHidden>
                   Reps
                 </Form.Label>
@@ -41,15 +39,11 @@ class Exercise extends Component {
                   <InputGroup.Text>
                     <i className="fa-solid fa-hashtag"></i>
                   </InputGroup.Text>
-                  <CounterInput />
+                  <Form.Control
+                    id="inlineFormInputGroupReps"
+                    placeholder="Reps"
+                  />
                 </InputGroup>
-              </Col>
-              <Col xs="auto" className="my-1">
-                <Form.Check
-                  type="checkbox"
-                  id="autoSizingCheck2"
-                  label="Complete"
-                />
               </Col>
               <Col xs="auto" className="my-1">
                 <Button type="submit">Submit</Button>
@@ -62,55 +56,4 @@ class Exercise extends Component {
   }
 }
 
-export default Exercise;
-=======
-class ExerciseInput extends Component {
-    render() { 
-        return (
-            <>
-                <Container>
-                    <Form>
-                        <Row className="align-items-center">
-                            <Col sm={4} className="my-1">
-                                <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
-                                    Exercise Name
-                                </Form.Label>
-                                <Form.Control id="inlineFormInputName" placeholder="Exercise Name" />
-                            </Col>
-                            <Col sm={2} className="my-1">
-                                <Form.Label htmlFor="inlineFormInputGroupWeight" visuallyHidden>
-                                    Weight
-                                </Form.Label>
-                                <InputGroup className="flex-nowrap">
-                                    <InputGroup.Text><i className="fa-solid fa-weight-hanging"></i></InputGroup.Text>
-                                    <Form.Control
-                                        id="inlineFormInputGroupWeight"
-                                        placeholder="Weight"
-                                    />
-                                </InputGroup>
-                            </Col>
-                            <Col sm={2} className="my-1">
-                                <Form.Label htmlFor="inlineFormInputGroupReps" visuallyHidden>
-                                    Reps
-                                </Form.Label>
-                                <InputGroup className="flex-nowrap">
-                                    <InputGroup.Text><i className="fa-solid fa-hashtag"></i></InputGroup.Text>
-                                    <Form.Control
-                                        id="inlineFormInputGroupReps"
-                                        placeholder="Reps"
-                                    />
-                                </InputGroup>
-                            </Col>
-                            <Col xs="auto" className="my-1">
-                                <Button type="submit">Submit</Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </Container>
-            </>
-        );
-    }
-}
-
 export default ExerciseInput;
->>>>>>> main:client/src/components/ExerciseInput.js
