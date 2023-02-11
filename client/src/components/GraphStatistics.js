4f746cfbbf11fd4679dad933a857815edf40bd1f
ethandb23 <119861532+ethandb23@users.noreply.github.com>
2023-02-11 02:41:59 -1000

    Update GraphStatistics.js

--- client/src/components/GraphStatistics.js
+++ client/src/components/GraphStatistics.js
@@ -1,51 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Line } from 'react-chartjs-2';
 
-// Define the WorkoutTracker component
-const WorkoutTracker = () => {
-  // Use the useState hook to store the data for the graph
-  const [data, setData] = useState({});
+// Define the GraphStatistics component as a class component
+class GraphStatistics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    };
+  }
 
-  // Use the useEffect hook to fetch the workout history data when the component is mounted
-  useEffect(() => {
+  componentDidMount() {
     // Replace this with code to fetch the user's workout history data from a backend API or database
     const workoutData = [1, 2, 3, 4, 5, 6, 7];
 
     // Update the component's state with the fetched workout history data
-    setData({
-      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-      datasets: [
-        {
-          label: 'Workout Intensity',
-          data: workoutData,
-          borderColor: 'rgba(75,192,192,1)',
-          backgroundColor: 'rgba(75,192,192,0.2)',
-          fill: true
-        }
-      ]
+    this.setState({
+      data: {
+        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        datasets: [
+          {
+            label: 'Workout Intensity',
+            data: workoutData,
+            borderColor: 'rgba(75,192,192,1)',
+            backgroundColor: 'rgba(75,192,192,0.2)',
+            fill: true
+          }
+        ]
+      }
     });
-  }, []);
+  }
 
   // Render the component
-  return (
-    <div>
-      {/* Render the line graph using the data stored in the component's state */}
-      <Line
-        data={data}
-        options={{
-          title: {
-            display: true,
-            text: 'Workout History',
-            fontSize: 20
-          },
-          legend: {
-            display: false
-          }
-        }}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        {/* Render the line graph using the data stored in the component's state */}
+        <Line
+          data={this.state.data}
+          options={{
+            title: {
+              display: true,
+              text: 'Workout History',
+              fontSize: 20
+            },
+            legend: {
+              display: false
+            }
+          }}
+        />
+      </div>
+    );
+  }
+}
 
-// Export the WorkoutTracker component
-export default WorkoutTracker;
+// Export the GraphStatistics component
+export default GraphStatistics;
