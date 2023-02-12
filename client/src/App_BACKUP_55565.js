43a1708c041901bb22351b8c56dd0f488dd01fbe
Selina Lavery <selina118@hotmail.com>
2023-02-12 17:42:40 +0000

    "Merge conflict resolved"

---
+++ client/src/App_BACKUP_55565.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Profile from "./pages/Profile";
+import { Route, Routes } from "react-router-dom";
+import Workout from "./pages/Workout";
+import GraphStatistics from "./components/GraphStatistics";
+
+import ProfilePicture from "./components/ProfilePicture";
+import usman_pic from "./userImage/usman_pic.png";
+
+class App extends Component {
+	state = {};
+	render() {
+		return (
+			<>
+				<NavBar />
+				<Routes>
+					{/* <Route exact path="/" element={<Login />} /> */}
+					<Route
+						path="/profile"
+						element={<Profile />}
+					/>
+					<Route
+						path="/workout"
+						element={<Workout />}
+					/>
+					<Route
+						path="/graph-statistics"
+						element={<GraphStatistics />}
+					/>
+				</Routes>
+
+				<ProfilePicture
+					src={usman_pic}
+					alt="Usman"
+					username="Us-Dawg"
+					totalWorkouts={1000}
+					workoutsThisWeek={5}
+				/>
+			</>
+		);
+	}
+}
+export default App;
