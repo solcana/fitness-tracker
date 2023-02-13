0c69d518afa5e1ddc767aef75bb6558cbb53b096
Hal Evans <hal.evans@newtoneurope.com>
2023-02-13 12:42:40 +0000

    centered addExerciseModal

--- client/src/pages/Workout.js
+++ client/src/pages/Workout.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Container } from "react-bootstrap";
-import ExerciseInputModal from "../components/ExerciseInputModal";
 import WorkoutContainer from "../components/WorkoutContainer";
 
 class Workout extends Component {
@@ -9,7 +8,6 @@ class Workout extends Component {
             <Container>
                 <h1>Workout Page</h1>
                 <WorkoutContainer />
-                <ExerciseInputModal />
             </Container>
         );
     }
