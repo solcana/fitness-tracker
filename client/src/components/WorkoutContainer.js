bd9f3ca479fab05623388bbacca1811560c6eadf
Ana Solcan <solcan.anna@gmail.com>
2023-02-14 12:34:02 +0200

    merged conflicts

--- client/src/components/WorkoutContainer.js
+++ client/src/components/WorkoutContainer.js
@@ -5,44 +5,30 @@ import ExerciseInputModal from "./ExerciseInputModal";
 import ExerciseItem from "./ExerciseItem";
 
 class WorkoutContainer extends Component {
-<<<<<<< HEAD
+  constructor(props) {
+    super(props);
+    this.state = {
+      exercises: [],
+    };
+  }
+
+  handleAddExercise = (exercise) => {
+    this.setState((prevState) => ({
+      exercises: [...prevState.exercises, exercise],
+    }));
+  };
+
   render() {
     return (
       <>
         <h5>WorkoutContainer</h5>
         <ExerciseItem />
         <div className="d-flex justify-content-center">
-          <ExerciseInputModal />
+          <ExerciseInputModal onAddExercise={this.handleAddExercise} />
         </div>
       </>
     );
   }
-=======
-    constructor(props) {
-        super(props);
-        this.state = {
-            exercises: []
-        };
-    }
-
-    handleAddExercise = (exercise) => {
-        this.setState(prevState => ({
-            exercises: [...prevState.exercises, exercise]
-        }));
-    }
-
-    render() { 
-        return (
-            <>
-                <h5>WorkoutContainer</h5>
-                <ExerciseItem />
-                <div className="d-flex justify-content-center">
-                    <ExerciseInputModal onAddExercise={this.handleAddExercise} />
-                </div>
-            </>
-        );
-    }
->>>>>>> 1ab86ab9b75c994cbd0a4ca7f4c1a57ecda530d6
 }
 
 export default WorkoutContainer;
