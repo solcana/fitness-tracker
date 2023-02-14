99eb71ae95829b8a5bfb3b49d93798010d0e1520
Selina Lavery <selina118@hotmail.com>
2023-02-14 11:55:08 +0000

    Add Register Modal component. Also added onChangePassword function to UserLogin component.

---
+++ client/src/components/UserRegisterModal.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class UserRegisterModal extends Component {
+	state = {};
+	render() {
+		return (
+			<container>
+				<h1>Register Modal</h1>
+			</container>
+		);
+	}
+}
+
+export default UserRegisterModal;
