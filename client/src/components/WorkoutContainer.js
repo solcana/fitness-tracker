a1c73eb50864922b4ac610f5ba51dfe37be227f2
Hal Evans <hal.evans@newtoneurope.com>
2023-02-14 11:54:43 +0000

    adding exercise now pushes exercise to last workout in database

--- client/src/components/WorkoutContainer.js
+++ client/src/components/WorkoutContainer.js
@@ -3,11 +3,14 @@
 import React, { Component } from "react";
 import ExerciseInputModal from "./ExerciseInputModal";
 import ExerciseItem from "./ExerciseItem";
+import axios from 'axios';
+import apiUrl from "./apiConfig";
 
 class WorkoutContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            latestWorkoutId: "",
             exercises: []
         };
     }
@@ -18,13 +21,27 @@ class WorkoutContainer extends Component {
         }));
     }
 
+    componentDidMount = () => {
+        // Get all workouts and select ID as last entry in database             // MAYBE CHANGE LATER TO BE LATEST CreatedAt DATE?
+        axios.get(apiUrl + "/workout")
+            .then(response => {
+                const latestWorkoutId = response.data.workouts[response.data.workouts.length - 1]._id;
+                this.setState({ latestWorkoutId: latestWorkoutId });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     render() { 
         return (
             <>
                 <h5>WorkoutContainer</h5>
                 <ExerciseItem />
                 <div className="d-flex justify-content-center">
-                    <ExerciseInputModal onAddExercise={this.handleAddExercise} />
+                    <ExerciseInputModal
+                        onAddExercise={this.handleAddExercise}
+                        latestWorkoutId={this.state.latestWorkoutId} />
                 </div>
             </>
         );
