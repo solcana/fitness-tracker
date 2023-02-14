391ded393367f0f9a59be88840c9c2fff6d37ade
Ana Solcan <solcan.anna@gmail.com>
2023-02-14 12:02:36 +0200

    merged conflicts

--- client/src/components/ExerciseInputModal.js
+++ client/src/components/ExerciseInputModal.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import { Button, Modal, Form, InputGroup, FormControl } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,26 +6,17 @@ import {
   faDumbbell,
   faHashtag,
 } from "@fortawesome/free-solid-svg-icons";
-=======
-import React, { Component } from 'react';
-import { Button, Modal, Form, InputGroup, FormControl } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileSignature, faDumbbell, faHashtag } from '@fortawesome/free-solid-svg-icons';
-import apiUrl from './apiConfig';
+import apiUrl from "./apiConfig";
 import axios from "axios";
->>>>>>> 1ab86ab9b75c994cbd0a4ca7f4c1a57ecda530d6
 
 class ExerciseInputModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
       show: false,
-<<<<<<< HEAD
-=======
       exerciseName: "",
       exerciseWeight: "",
-      exerciseReps: ""
->>>>>>> 1ab86ab9b75c994cbd0a4ca7f4c1a57ecda530d6
+      exerciseReps: "",
     };
   }
 
@@ -34,9 +24,7 @@ class ExerciseInputModal extends Component {
     this.setState({ show: true });
     console.log(apiUrl + "/workout");
 
-    axios
-        .get(apiUrl + "/workout")
-        .then((res) => console.log(res));
+    axios.get(apiUrl + "/workout").then((res) => console.log(res));
   };
 
   handleClose = () => {
@@ -57,42 +45,41 @@ class ExerciseInputModal extends Component {
 
   handleAddExercise = () => {
     const exercise = {
-        exerciseName: this.state.exerciseName, 
-        exerciseWeight: this.state.exerciseWeight, 
-        exerciseReps: this.state.exerciseReps
-    }
+      exerciseName: this.state.exerciseName,
+      exerciseWeight: this.state.exerciseWeight,
+      exerciseReps: this.state.exerciseReps,
+    };
 
     this.props.onAddExercise(exercise);
 
-    axios.post(apiUrl + "/workout", {
+    axios
+      .post(apiUrl + "/workout", {
         startDate: Date.now(),
         completed: true,
         exercises: [
-            {
-                name: "New Squat",
-                weight: 20,
-                reps: 10,
-                completed: true
-            }
-        ]
-    })
-    .then(response => {
+          {
+            name: "New Squat",
+            weight: 20,
+            reps: 10,
+            completed: true,
+          },
+        ],
+      })
+      .then((response) => {
         console.log(response);
-    })
-    .catch(error => {
+      })
+      .catch((error) => {
         if (error.response) {
-            //response status is an error code
-            console.log(error.response.status);
+          //response status is an error code
+          console.log(error.response.status);
+        } else if (error.request) {
+          //response not received though the request was sent
+          console.log(error.request);
+        } else {
+          //an error occurred when setting up the request
+          console.log(error.message);
         }
-        else if (error.request) {
-            //response not received though the request was sent
-            console.log(error.request);
-        }
-        else {
-            //an error occurred when setting up the request
-            console.log(error.message);
-        }
-    });
+      });
 
     this.setState({ show: false });
   };
@@ -114,18 +101,17 @@ class ExerciseInputModal extends Component {
               <Form.Group controlId="formExerciseName">
                 <Form.Label>Exercise Name</Form.Label>
                 <InputGroup>
-<<<<<<< HEAD
                   <InputGroup.Text>
                     <FontAwesomeIcon
                       icon={faFileSignature}
                       style={{ width: "16px" }}
                     />
                   </InputGroup.Text>
-                  <FormControl type="name" placeholder="Enter exercise name" />
-=======
-                  <InputGroup.Text><FontAwesomeIcon icon={faFileSignature} style={{ width: "16px" }} /></InputGroup.Text>
-                  <FormControl type="name" placeholder="Enter exercise name" onChange={this.updateName} />
->>>>>>> 1ab86ab9b75c994cbd0a4ca7f4c1a57ecda530d6
+                  <FormControl
+                    type="name"
+                    placeholder="Enter exercise name"
+                    onChange={this.updateName}
+                  />
                 </InputGroup>
                 <Form.Text className="text-muted">
                   Enter any name for your exercise.
@@ -135,7 +121,6 @@ class ExerciseInputModal extends Component {
               <Form.Group controlId="formExerciseWeight">
                 <Form.Label>Weight</Form.Label>
                 <InputGroup>
-<<<<<<< HEAD
                   <InputGroup.Text>
                     <FontAwesomeIcon
                       icon={faDumbbell}
@@ -145,18 +130,14 @@ class ExerciseInputModal extends Component {
                   <FormControl
                     type="weight"
                     placeholder="Enter exercise weight (kg)"
+                    onChange={this.updateWeight}
                   />
-=======
-                  <InputGroup.Text><FontAwesomeIcon icon={faDumbbell} style={{ width: "16px" }} /></InputGroup.Text>
-                  <FormControl type="weight" placeholder="Enter exercise weight (kg)" onChange={this.updateWeight} />
->>>>>>> 1ab86ab9b75c994cbd0a4ca7f4c1a57ecda530d6
                 </InputGroup>
               </Form.Group>
 
               <Form.Group controlId="formExerciseWeight">
                 <Form.Label>Reps</Form.Label>
                 <InputGroup>
-<<<<<<< HEAD
                   <InputGroup.Text>
                     <FontAwesomeIcon
                       icon={faHashtag}
@@ -166,11 +147,8 @@ class ExerciseInputModal extends Component {
                   <FormControl
                     type="reps"
                     placeholder="Enter exercise reps (#)"
+                    onChange={this.updateReps}
                   />
-=======
-                  <InputGroup.Text><FontAwesomeIcon icon={faHashtag} style={{ width: "16px" }}/></InputGroup.Text>
-                  <FormControl type="reps" placeholder="Enter exercise reps (#)" onChange={this.updateReps} />
->>>>>>> 1ab86ab9b75c994cbd0a4ca7f4c1a57ecda530d6
                 </InputGroup>
               </Form.Group>
             </Form>
