fc84c6a94c1db434c4de7f91b710a2c0eb2cf382
ethandb23 <119861532+ethandb23@users.noreply.github.com>
2023-02-15 00:33:19 -1000

    Graph Render Instead Of Placeholder

--- client/src/components/GraphStatistics.js
+++ client/src/components/GraphStatistics.js
@@ -1,61 +1,54 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Container, Form, Button } from 'react-bootstrap';
 
-class GraphStatistics extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {}
-    };
-  }
+function Graph() {
+  const [data, setData] = useState([]);
 
-  componentDidMount() {
-    // Replace this with code to fetch the user's workout history data from a backend API or database
-    const workoutData = [1, 2, 3, 4, 5, 6, 7];
+  const handleAddData = (e) => {
+    e.preventDefault();
+    const newDate = e.target.date.value;
+    const newWorkout = e.target.workout.value;
+    const newReps = parseInt(e.target.reps.value);
+    setData([...data, { date: newDate, workout: newWorkout, reps: newReps }]);
+    e.target.reset();
+  };
 
-    // Update the component's state with the fetched workout history data
-    this.setState({
-      data: {
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        datasets: [
-          {
-            label: 'Workout Intensity',
-            data: workoutData,
-            borderColor: 'rgba(75,192,192,1)',
-            backgroundColor: 'rgba(75,192,192,0.2)',
-            fill: true
-          }
-        ]
-      }
-    });
-  }
-
-  render() {
-    return (
-      <Container className="mt-4">
-        <Row>
-          <Col>
-            <div
-              className="p-3 mb-5 bg-white rounded"
-              style={{ height: "500px", border: "1px solid #ccc" }}
-            >
-              {/* Placeholder graph */}
-              <div
-                style={{
-                  height: "100%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center"
-                }}
-              >
-                <h3>Graph will be rendered here</h3>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <h1>Usman's Workout History</h1>
+      <Form onSubmit={handleAddData}>
+        <Form.Group>
+          <Form.Label>Date</Form.Label>
+          <Form.Control type="date" name="date" required />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Workout Name</Form.Label>
+          <Form.Control type="text" name="workout" required />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Reps</Form.Label>
+          <Form.Control as="select" name="reps">
+            {[...Array(50)].map((_, i) => (
+              <option key={i + 1}>{i + 1}</option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+        <Button type="submit">Add Data</Button>
+      </Form>
+      <br />
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={data}>
+          <XAxis dataKey="date" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="reps" stroke="#8884d8" activeDot={{ r: 8 }} />
+        </LineChart>
+      </ResponsiveContainer>
+    </Container>
+  );
 }
 
-export default GraphStatistics;
+export default Graph;
--- server/server.js
+++ server/server.js
@@ -15,11 +15,11 @@ const user = require("./seedData/userSeed");
 
 // Connect to MongoDB database
 mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 mongoose.connection.once("open", () =>
-  console.log(`Connection to MongoDB on ${db}`)
+	console.log(`Connection to MongoDB on ${db}`)
 );
 
 //Require Passport Strategy and Options
@@ -34,7 +34,6 @@ const exerciseRouter = require("../server/routes/exerciseRoutes");
 const indexRouter = require("../server/routes/index");
 
 const userRouter = require("./routes/userRoutes"); //SL here***
-const User = require("./models/user");
 
 // Instantiate Express Application Object
 const app = express();
@@ -45,7 +44,7 @@ const reactPort = 3000;
 
 //Set CORS headers on response from this API using the 'cors' NPM package
 app.use(
-  cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}` })
+	cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}` })
 );
 
 //Define our suth strategy from before
@@ -65,40 +64,38 @@ app.use(userRouter); //SL here ****
 
 //temp test route
 app.get("/test", (req, res) => {
-  bcrypt.hash("1234", saltRounds, (error, hash) => {
-    res.status(200).json({ password: hash });
-  });
+	bcrypt.hash("1234", saltRounds, (error, hash) => {
+		res.status(200).json({ password: hash });
+	});
 });
 
 // Login Route
 app.post("/api/login", (req, res) => {
-	// verify that they are supplying username and password
+	//verify that they are supplying username and password
 	if (req.body.username && req.body.password) {
-		// search for the user in the database
-		User.findOne(
-			{ username: req.body.username, password: req.body.password },
-			(err, user) => {
-				if (err) {
-					console.log(err);
-					return res.status(500).json({ error: "Server error" });
-				}
-				if (!user) {
-					return res
-						.status(401)
-						.json({ error: "Invalid username or password" });
-				}
-				// generate and send JWT
-				const payload = {
-					response: "Login successful",
-					// try to keep as bare minimum as possible
-					// id: user.id,
-				};
-				const token = jwt.sign(payload, jwtOptions.secretOrKey, {
-					expiresIn: 600,
-				});
-				res.status(200).json({ success: true, token: token });
-			}
-		);
+		//This should be a Database call...
+		//
+		//Example: User.find({username: req.body.username})
+		if (
+			req.body.username === user.username &&
+			req.body.password === user.password
+		) {
+			//Select the information we want to send to the user
+			const payload = {
+				//try to keep as bare minimum as poss
+				id: user.id,
+			};
+
+			//Build a JSON Web Token using the paylosd
+			const token = jwt.sign(payload, jwtOptions.secretOrKey, {
+				expiresIn: 600,
+			}); // token expires in 10 minutes
+
+			//Send the jsoin web token back to the user
+			res.status(200).json({ success: true, token: token });
+		} else {
+			res.status(401).json({ error: "Invalid username or password" });
+		}
 	} else {
 		res.status(400).json({ error: "Username & Password Required" });
 	}
@@ -106,14 +103,14 @@ app.post("/api/login", (req, res) => {
 
 //dummy path to protect site form invalid
 app.get(
-  "/api/protected",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.status(200).json({
-      message: "Hey, you can see this message with the JSon Web Token.",
-      user: req.user,
-    });
-  }
+	"/api/protected",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		res.status(200).json({
+			message: "Hey, you can see this message with the JSon Web Token.",
+			user: req.user,
+		});
+	}
 );
 
 // Returns a statement saying the App is listening on our specified port
