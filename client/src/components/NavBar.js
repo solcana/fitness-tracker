7c7f4ce67794a3bc8fc621ae62653ecddd5d81cb
Selina Lavery <selina118@hotmail.com>
2023-02-16 07:21:06 +0000

    Add navbar toggle to update when user sign in

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
 	constructor(props) {
@@ -9,6 +10,7 @@ class NavBar extends Component {
 
 		this.state = {
 			isLoggedIn: false,
+			username: "",
 		};
 	}
 
@@ -34,23 +36,50 @@ class NavBar extends Component {
 				bg="dark"
 				variant="dark">
 				<Container>
-					<Navbar.Brand href="/">Fitness Tracker</Navbar.Brand>
+					<Navbar.Brand href="/profile">Fitness Tracker</Navbar.Brand>
 					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 					<Navbar.Collapse id="responsive-navbar-nav">
 						<Nav className="me-auto">
-							<Nav.Link href="/profile">Profile</Nav.Link>
-							<Nav.Link href="/workout">Workout</Nav.Link>
-							<Nav.Link href="/graph-statistics">Graph Stats</Nav.Link>
-							<Nav.Link href="/profile-picture">User Profile</Nav.Link>
+							<Nav.Link
+								as={Link}
+								to="/profile">
+								Profile
+							</Nav.Link>
+							<Nav.Link
+								as={Link}
+								to="/workout">
+								Workout
+							</Nav.Link>
+							<Nav.Link
+								as={Link}
+								to="/graph-statistics">
+								Graph Stats
+							</Nav.Link>
+							<Nav.Link
+								as={Link}
+								to="/profile-picture">
+								User Profile
+							</Nav.Link>
 							{this.state.isLoggedIn ? (
-								<Nav.Link onClick={this.handleLogout}>Sign Out</Nav.Link>
+								<Nav.Link
+									as={Link}
+									to="/user"
+									onClick={this.handleLogout}>
+									Sign Out
+								</Nav.Link>
 							) : (
-								<Nav.Link href="/user">Login</Nav.Link>
+								<Nav.Link
+									as={Link}
+									to="/user">
+									Login
+								</Nav.Link>
 							)}
 						</Nav>
-						<Navbar.Text>
-							Signed in as: <a href="#login">Usman's Gainz</a>
-						</Navbar.Text>
+						{this.state.isLoggedIn && (
+							<Navbar.Text>
+								Signed in as: <a href="#login">{this.state.username}</a>
+							</Navbar.Text>
+						)}
 					</Navbar.Collapse>
 				</Container>
 			</Navbar>
